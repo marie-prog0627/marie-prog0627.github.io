7acc479b19a13e1e6a570c05f6ee2ebc062514da
marie-prog0627 <marie.726project@gmail.com>
2019-11-14 16:03:35 +0900

    base web site with vuetify

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="gray accent-4"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-btn
+          href="https://marie-prog0627.github.io"
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Ryota Sakuma</span>
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <MyHome/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import MyHome from './components/MyHome';
+
+export default {
+  name: 'App',
+
+  components: {
+    MyHome,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/MyHome.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container>
+    <v-layout
+      text-left
+      wrap
+      align-center
+    >
+      <v-flex xs3>
+        <v-img
+          :src="require('../assets/icon.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+
+      </v-flex>
+
+      <v-flex
+        xs8
+        mb-5
+      >
+        <h1 class="display-2 font-weight-bold mb-3">
+          Profile
+        </h1>
+        <p class="subheading font-weight-bold">
+          I am Ryota Sakuma, a student in College of Information Science, University of Tsukuba.
+          <br>I research about haptics and develop support systems using haptic technology for people with impairments.
+        </p>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Achievement</h2>
+        <ul class="subheading font-weight-bold">
+          <li>2019.10 Released a paper at UIST 2019 posters</li>
+          <li>2019.07 Released a paper at Work in Progress of World Haptics Conference 2019</li>
+          <li>2018.12 Presented a poster at HCG 2018 (Domestic Conference)</li>
+        </ul>
+      </v-flex>
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">History</h2>
+
+        <ul class="subheading font-weight-regular">
+          <li>2018.03 Graduated from Kaichi high school</li>
+          <li>2019.04 Entered Colleage of Information Science, University of Tsukuba</li>
+        </ul>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Researches</h2>
+        <p class = "subheading font-weight-regular">
+          Work in Progress
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'MyHome',
+};
+</script>
